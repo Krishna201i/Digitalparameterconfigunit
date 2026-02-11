27e508d0072835fea87af25a097e83cb8075a508
Krishna201i <krishna.singh_cs.aiml24@gla.ac.in>
2026-02-11 13:57:34 +0530

    Initialize OLED display in setup and add display configuration

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_ADDR 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 void setup() {
-// write your initialization code here
+Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDR)) {
+        Serial.println(F("OLED not found "));
+        while (true);
+    }
 }
 
 void loop() {
